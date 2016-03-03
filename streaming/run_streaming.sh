c851e948d721df71b1b08d13f020ef0a5cbde34d
Zoltan Toth <tz@looper.hu>
2016-03-03 15:45:35 +0100

    updated filename in streaming command

--- streaming/run_streaming.sh
+++ streaming/run_streaming.sh
@@ -1,1 +1,1 @@
-hadoop jar streaming.jar -input birdstrikes.csv -output counts4 -mapper mapper.sh -reducer reducer.py -numReduceTasks 10 -file mapper.sh -file reducer.py
+hadoop jar streaming.jar -input birdstrikes.csv -output counts.txt -mapper mapper.sh -reducer reducer.py -numReduceTasks 10 -file mapper.sh -file reducer.py
