ff4866da4504ebb8d27c7e3f583e59f4475c23a4
Zoltan C. Toth <tz@looper.hu>
2016-05-08 17:49:41 +0200

    passz

--- spark/show-data.R
+++ spark/show-data.R
@@ -1,4 +1,4 @@
-install.packages("ggplot2")
-data = read.table("data/logreg.txt", sep=" ", header=FALSE, col.names = c("label", "x", "y"))
+#install.packages("ggplot2")
 library(ggplot2)
+data = read.table("/Users/zoltanctoth/src/my/bigdata-training/spark/data/logreg2.txt", sep=" ", header=FALSE, col.names = c("label", "x", "y"))
 qplot(x=x, y=y, colour=label, data=data)
