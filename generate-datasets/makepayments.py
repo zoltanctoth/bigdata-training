296e6c8d81951b83244f4125e72a20e353911d3b
Zoltan C. Toth <zoltan@datapao.com>
2017-02-16 11:02:01 +0100

    update some dataset generation scripts

--- generate-datasets/makepayments.py
+++ generate-datasets/makepayments.py
@@ -4,15 +4,17 @@ import datetime
 from datetime import timedelta
 
 random.seed(2888)
-date = datetime.date(2015, 1, 1)
-# print "date,user_id,amount"
+date = datetime.date(2017, 1, 1)
+
+print "date,user_id,amount,card"
 while True:
-    user_id = random.randint(1, 100000)
+    user_id = random.randint(1, 600000)
     payment = random.randint(1, 4) * 50
+    card = ["MasterCard","Visa","AMEX"][random.randint(0,2)]
 
     if random.randint(0, 30000) == 0:
         date = date + timedelta(days=1)
     if date >= datetime.date.today():
         sys.exit(0)
 
-    print "%s,%d,%d" % (date, user_id, payment)
+    print "%s,%d,%d,%s" % (date, user_id, payment, card)
--- generate-datasets/makeusers.py
+++ generate-datasets/makeusers.py
@@ -1,9 +1,10 @@
 import random
 
-cities = ["Budapest", "Budapest", "Budapest", "Debrecen", "Debrecen", "Sopron", "Gyor"]
+cities = ["Budapest", "Budapest", "Budapest", "Amsterdam", "Amsterdam", "Sopron", "Vienna"]
 random.seed(288)
 
-# print "user_id,city"
-for user_id in range(1, 100000):
+print "user_id,city,age"
+for user_id in range(1, 600000):
     city = cities[random.randint(0, len(cities) - 1)]
-    print "%d,%s" % (user_id, city)
+    age = random.randint(19,75)
+    print "%d,%s,%d" % (user_id, city, age)
